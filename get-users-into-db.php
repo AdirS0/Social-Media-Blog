bc08a187c8e0c77455ee0d3a405ca9609fb00d7f
Adir Solomon <79449128+AdirS0@users.noreply.github.com>
2023-05-16 06:54:34 +0300

    Improving get-users-into-db.php

--- get-users-into-db.php
+++ get-users-into-db.php
@@ -1,18 +1,23 @@
 <?php
 
+require "classes/User.php";
+require "classes/Database.php";
+
 $ch = curl_init();
 $url = "https://jsonplaceholder.typicode.com/users";
+$table = 'users';
 
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
 
-if ($e = curl_error($ch)){
+if ($e = curl_error($ch)) {
     die("Error curling data: $e");
-} else{
+} else {
     $usersData = json_decode($response, true);
-    print_r($usersData);
+    
+
 }
 
 curl_close($ch);
