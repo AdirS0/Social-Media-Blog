1b62a6f7cf46d402ffef7b662fc1fe17596417d7
Adir Solomon <79449128+AdirS0@users.noreply.github.com>
2023-05-16 22:40:44 +0300

    image curl

---
+++ get-image.php
@@ -0,0 +1,15 @@
+<?php
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://cdn2.vectorstock.com/i/1000x1000/23/81/default-avatar-profile-icon-vector-18942381.jpg");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$imageData = curl_exec($ch);
+
+curl_close($ch);
+
+file_put_contents("image.jpg", $imageData);
+
+echo "<img src='image.jpg' alt='Image'>\n";
+
+
