7065b07f7864c10f6bf8d868b6301ca42726bb5f
Adir Solomon <79449128+AdirS0@users.noreply.github.com>
2023-05-16 06:54:10 +0300

    Improving get-posts-into-db

--- get-posts-into-db.php
+++ get-posts-into-db.php
@@ -1,18 +1,23 @@
 <?php
 
+require "classes/Post.php";
+require "classes/Database.php";
+
 $ch = curl_init();
 $url = "https://jsonplaceholder.typicode.com/posts";
+$table = 'posts';
 
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
 
-if ($e = curl_error($ch)){
+if ($e = curl_error($ch)) {
     die("Error curling data: $e");
-} else{
+} else {
     $postsData = json_decode($response, true);
-    print_r($postsData);
+    
+    
 }
 
 curl_close($ch);
