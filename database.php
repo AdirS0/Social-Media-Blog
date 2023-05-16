5c0b772fc492b5da5cbdd13b0f8718810cd89bdb
Adir Solomon <79449128+AdirS0@users.noreply.github.com>
2023-05-16 22:41:11 +0300

    database file with parameters for DB

---
+++ database.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'classes/Database.php';
+
+$host = "localhost";
+$username = "adir";
+$password = "142536";
+$database = "proj_db";
+
+$db = new Database($host, $username, $password, $database);
