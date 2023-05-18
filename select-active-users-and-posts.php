78455dc3db332d1d5ca7bde0724187d4c987500b
Adir Solomon <adir199@gmail.com>
2023-05-18 13:15:50 +0300

    Improved social media UI

--- select-active-users-and-posts.php
+++ select-active-users-and-posts.php
@@ -1,7 +1,6 @@
 <?php
 
 require "database.php";
-require "get-image.php";
 
 $results = $db->selectActiveUsersAndPosts();
 $username = $results[0]['username'];
@@ -9,17 +8,18 @@ $email = $results[0]['email'];
 
 echo '<div>';
 echo '<div>';
+echo '<img src="image.jpg" alt="User Image" width=150 height=150>';
 echo '<h1>' . $username . ' ' . $email . '</h1>';
 foreach ($results as $index => $row) {
     $closeDivFlag = 0;
     if ($username !== $row['username']) {
         echo '</div>';
-        
+
         $username = $row['username'];
         $email = $row['email'];
 
         echo '<div>';
-        echo '<img src="image.jpg" alt="User Image">';
+        echo '<img src="image.jpg" alt="User Image" width=150 height=150>';
         echo '<h1>' . $username . ' ' . $email . '</h1>';
     }
 
@@ -27,8 +27,7 @@ foreach ($results as $index => $row) {
     $postTitle = $row['title'];
     $postBody = $row['body'];
 
-    echo '<h4>' . $publishedAt . '</h4>';
-    echo '<h2>' . $postTitle . '</h2>';
+    echo '<h3>'. $publishedAt . ' : ' . $postTitle . '</h3>';
     echo '<p>' . $postBody . '</p>';
 }
 echo '</div>';
